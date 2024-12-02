fb773621af48d40b52bc9904fc3ce471fe92eea2
SH <1509929149@qq.com>
2024-12-02 22:21:09 +0800

    fix: update training scripts.

--- train_scripts/router_train.sh
+++ train_scripts/router_train.sh
@@ -7,12 +7,11 @@ learning_rate="5e-5"
 tempreture=1
 similarity_function="cos"
 sample_loss_weight=0
-regular_loss_weight=0
 cluster_loss_weight=1
 seeds=(5)
 
 for seed in "${seeds[@]}"; do
-    python -u train_router_mdeberta.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 50 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --regular_loss_weight ${regular_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --final_eval --seed ${seed} --batch_size 16 \
+    python -u train_router_mdeberta.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 50 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --final_eval --seed ${seed} --batch_size 16 \
     --data_paths "./datasets/split2_model7_cluster/gsm8k-train.json" "./datasets/split2_model7_cluster/humaneval_train.json"  "./datasets/split2_model7_cluster/arc_challenge_train.json" "./datasets/split2_model7_cluster/cmmlu_train.json" "./datasets/split2_model7_cluster/mmlu_train.json" \
     --test_data_paths "./datasets/split2_model7/gsm8k-test.json" "./datasets/split2_model7/humaneval_test.json" "./datasets/split2_model7/arc_challenge_test.json" "./datasets/split2_model7/cmmlu_test.json" "./datasets/split2_model7/mmlu_test.json" \
     --save_path "/data/home/chensh/projects/LLM_router/logs/paper_result/RouterDC/clw_${cluster_loss_weight}/slw_${sample_loss_weight}_clw_${cluster_loss_weight}_${similarity_function}_tk_${top_k}_lk_${last_k}_lr_${learning_rate}_step_${training_steps}_t_${tempreture}_seed_${seed}" \
--- train_scripts/router_train_routerbench.sh
+++ train_scripts/router_train_routerbench.sh
@@ -8,7 +8,6 @@ learning_rate="5e-5"
 tempreture=1
 similarity_function="cos"
 sample_loss_weight=0
-regular_loss_weight=0
 cluster_loss_weight=1
 seeds=(7)
 cost_rates=(0 1)
@@ -17,7 +16,7 @@ for cost_rate in "${cost_rates[@]}"; do
     for top_k in "${top_ks[@]}"; do
         for last_k in "${last_ks[@]}"; do
             for seed in "${seeds[@]}"; do
-                python -u train_router_mdeberta_routerbench.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 100 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --regular_loss_weight ${regular_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --seed ${seed} --final_eval --cost_rate ${cost_rate} \
+                python -u train_router_mdeberta_routerbench.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 100 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --seed ${seed} --final_eval --cost_rate ${cost_rate} \
                 --data_paths './datasets/routerbench_cluster/gsm8k_train.csv' './datasets/routerbench_cluster/hellaswag_train.csv' './datasets/routerbench_cluster/mbpp_train.csv' './datasets/routerbench_cluster/mmlu_train.csv' './datasets/routerbench_cluster/winograde_train.csv' './datasets/routerbench_cluster/arc_challenge_train.csv' \
                 --test_data_paths './datasets/routerbench_zs/gsm8k_test.csv' './datasets/routerbench_zs/hellaswag_test.csv' './datasets/routerbench_zs/mbpp_test.csv' './datasets/routerbench_zs/mmlu_test.csv' './datasets/routerbench_zs/winograde_test.csv' './datasets/routerbench_zs/arc_challenge_test.csv' \
                 --save_path "/data/home/chensh/projects/LLM_router/logs/paper_result/routerbench/cr_${cost_rate}_slw_${sample_loss_weight}_tk_${top_k}_lk_${last_k}_lr_${learning_rate}_step_${training_steps}_t_${tempreture}_seed_${seed}" \
